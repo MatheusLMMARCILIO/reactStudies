2af1ca4764829646a38a7f56e59744bc4f92e613
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2025-06-18 01:27:52 -0300

    okay

--- src/pages/useStates.jsx
+++ src/pages/useStates.jsx
@@ -5,6 +5,21 @@ import { useState } from "react"
 function Use() {
 
     const [cor, setColor] = useState('')
+    const [contar, setcontar] = useState(0)
+    const [mostrar, setMostrar] = useState(true)
+    const [nome, setNome] = useState('')
+
+    //atividades
+
+    const [texto, setTexto] = useState('Olá')
+
+    function MudarTexto() {
+        setTexto(texto === 'Olá!' ? 'Tchau' : 'Olá!')
+    }
+
+    const [color, setColorr] = useState('')
+
+const [aceito, setAceito] = useState(false)
 
     return (
         <section>
@@ -18,8 +33,66 @@ function Use() {
             <button onClick={() => { setColor("Black") }} >Black</button>
             <button onClick={() => { setColor("") }} >Limpar</button>
 
+            <h2>contador</h2>
+
+            <p>Valor: {contar}</p>
+
+            <button onClick={() => { setcontar(contar + 1) }}>+</button>
+            <button onClick={() => { setcontar(contar - 1) }} >-</button>
+
+            <h2>Ocular ou Mostrar</h2>
+
+            {mostrar && <p>Esse texto aparece e desaparece</p>}
+
+            <button onClick={() => setMostrar(!mostrar)}>
+                {mostrar ? 'Ocultar' : 'Mostrar'}
+            </button>
+
+            <h1>Atualizar texto</h1>
+
+            <input type="text" placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+
+            <p>Seu nome é: {nome}</p>
+
+            <h2>Mudar texto</h2>
+
+            <p>{texto}</p>
+
+            <button onClick={MudarTexto} >Trocar texto</button>
+
+            <h2>Mudar fundo do bgc</h2>
+
+            <div style={{ width: 50, height: 50, backgroundColor: color }} ></div>
+
             <br />
-            <br />
+
+            <button onClick={() => setColorr("Blue")} >Azul</button>
+            <button onClick={() => setColorr("Pink")} >Pink</button>
+            <button onClick={() => setColorr('Green')} >Green</button>
+
+            <h1>Termos de uso</h1>
+
+           <label>
+<input type="checkbox" checked={aceito} onChange={() => {setAceito(!aceito)}} />
+
+           </label>
+{aceito && <p>E permitido</p>}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
